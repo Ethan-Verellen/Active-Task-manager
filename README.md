4ed9bb561849b71c305b2097f470b0f35e509ef5
Ethan Verellen <ethanverellen@gmail.com>
2024-08-24 16:04:10 -0400

    Most of the website is functional

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Active-Task-manager
+
+The task manager is a bit of code that was ment to allow one to manage their tasks, but at the current point it is incompleate and messed up.  
+
+## Installation
+
+In order to install, either zip install it and unzip it or follow this links instruction:
+
+https://docs.github.com/en/get-started/start-your-journey/downloading-files-from-github
+
+## Description
+This is a way to write down what happens to you in a way that saves it, but does not put it on the internet, and instead it is saved to your personal device.
+
+## Visuals
+![This is a image of the full page](./Assets/images/full.png)
+
+![this is an image of the dialog](./Assets/images/dialog.png)
+
+## Link
+
+https://ethan-verellen.github.io/Self-blog/
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,109 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks"))||[];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const element = document.getElementById("add")
+const e2 = document.getElementById("taskboard")
+const tc = $('#todo-cards');
+const pc = $('#in-progress-cards');
+const dc = $('#done-cards');
+let i = 0; 
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+let id = taskList.length + 1;
+localStorage.setItem('nextId', id)
 }
 
+  
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
+function createTaskCard(task) {   
+    const cc = $('<p>');
+    const ct = (task.name + "\n" + task.date + "\n" + task.description);
+    const card = $('<section>');
+    cc.addClass('card').text(ct);
+    // const cd = $('<button>').addClass('delete').text('Delete')
+    card.append(cc);
+    card.draggable();
+    // cd.addEventListener("click", handleDeleteTask)
+    // The delete button and droppable do not work
+    $( "#in-progress-cards" ).droppable({
+        drop: function (event, ui) {
+            console.log('works')
+          }
+        });
+    return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].location == "todo") {
+        const test = createTaskCard(taskList[i]);
+        tc.append(test);
+    }
+     else if (taskList.location == "progress") {
+        const test = createTaskCard(taskList[i]);
+        pc.append(test);
+     }
+     else {
+        const test = createTaskCard(taskList[i]);
+        cc.append(test);
+     }
+}
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+    generateTaskId()
+    $("#dialog").dialog();
+    e2.addEventListener("submit", function handleAddTaskp2(event){
+        event.preventDefault();
+        const tn = document.getElementById('name').value;
+        const d = document.getElementById('date').value;
+        const de = document.getElementById('description').value;
+        if (tn == "" || d == "" || de == "") {
+            alert('Please fill in info.');
+            return;
+        }
+        const temptask = {
+            name: tn,
+            date: d,
+            description: de,
+            id: nextId,
+            location: 'todo',
+        }
+        taskList.push(temptask);
+        const stask = JSON.stringify(taskList);
+        localStorage.setItem('tasks', stask)
+        $("#dialog").dialog('close');
+        renderTaskList()
+    })
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+    console.log("test")
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+console.log("test")
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
-});
+    renderTaskList()
+    generateTaskId()
+    element.addEventListener("click", handleAddTask)
+    $('#date').datepicker({
+        changeMonth: true,
+        changeYear: true,
+      });
+      $( "#in-progress-cards" ).droppable({
+        accept: '.card',
+        drop: function (event, ui) {
+            console.log('works')
+          }
+        });
+      });
